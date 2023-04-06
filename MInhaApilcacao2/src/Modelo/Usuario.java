f3a7b1d8c704c554571f113ad301745f637b4675
LordMark <mv20040908@gmail.com]>
2023-04-05 22:26:33 -0300

    Projeto ~cadastro usuario

---
+++ MInhaApilcacao2/src/Modelo/Usuario.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+public class Usuario {
+
+    Long id;
+    String cpf;
+    String email;
+    String telefone;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail() {
+        this.email = email;
+    }
+
+    public Long getid() {
+        return id;
+    }
+
+    public void setNome(String nome) {
+        this.id = id;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
+
