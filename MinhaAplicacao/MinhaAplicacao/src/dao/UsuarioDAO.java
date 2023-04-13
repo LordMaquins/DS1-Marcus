da3672c224a473432719b144f136015916e26d67
LordMark <mv20040908@gmail.com>
2023-04-12 21:45:30 -0300

    Projeto de cadastro com banco de dados em java

---
+++ MinhaAplicacao/MinhaAplicacao/src/dao/UsuarioDAO.java
@@ -0,0 +1,106 @@
+package dao;
+import factory.ConnectionFactory;
+import modelo.Usuario;
+import java.sql.*;
+import java.sql.PreparedStatement;
+
+public class UsuarioDAO {
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String telefone;
+    private final Connection connection;
+    
+    public UsuarioDAO(){
+        this.connection = new ConnectionFactory().getConnection();
+    }
+    
+    public void adiciona(Usuario usuario){
+        
+        String sql = "INSERT INTO usuario(nome,cpf,email,telefone)VALUES(?,?,?,?)";
+        try{
+            
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1, usuario.getNome());
+            stmt.setString(2, usuario.getCpf());
+            stmt.setString(3, usuario.getEmail());
+            stmt.setString(4, usuario.getTelefone());
+            stmt.execute();
+            stmt.close();
+        } catch (SQLException u){
+            throw new RuntimeException(u);
+        }
+    }
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the cpf
+     */
+    public String getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the telefone
+     */
+    public String getTelefone() {
+        return telefone;
+    }
+
+    /**
+     * @param telefone the telefone to set
+     */
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
+}
---
+++ MinhaAplicacao/MinhaAplicacao/src/minhaaplicacao/MinhaAplicacao.java
@@ -0,0 +1,10 @@
+
+package minhaaplicacao;
+
+public class MinhaAplicacao {
+
+    public static void main(String[] args) {
+
+    }
+    
+}
